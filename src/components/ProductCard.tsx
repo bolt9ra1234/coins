31305774e6c649daa9a87954d152a399a2740681
bolt9ra1234 <bolt9ra1234@mail.ru>
2025-09-24 18:03:06 +0300

    Updated ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -47,7 +47,7 @@ const PriceText = styled(Typography)(({ theme }) => ({
 
 const BuyButton = styled(Button)(({ theme }) => ({
   width: '100%',
-  height: 60,
+  height: 40,
   borderRadius: 35,
   backgroundColor: '#deb544',
   color: '#111111',
