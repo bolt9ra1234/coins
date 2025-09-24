e49d8c70fcb23567fed62794f5aa887caed91ee8
bolt9ra1234 <bolt9ra1234@mail.ru>
2025-09-24 16:54:04 +0300

    Запуск приложения с альтернативной конфигурацией

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react', 'scheduler'],
+    disabled: true,
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
   },
 });
