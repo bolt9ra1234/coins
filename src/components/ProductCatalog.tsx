8e72810c8f20b00c9eed13c1163a4290a6a82915
bolt9ra1234 <bolt9ra1234@mail.ru>
2025-09-24 17:44:03 +0300

    Updated ProductCatalog.tsx

--- src/components/ProductCatalog.tsx
+++ src/components/ProductCatalog.tsx
@@ -85,7 +85,7 @@ const ProductCatalog: React.FC<ProductCatalogProps> = ({ searchQuery }) => {
         {filteredProducts.map((product) => (
           <Grid 
            item 
-  xs={12}
+  xs={12} 
   sm={6}
   md={4}
   lg={3}
