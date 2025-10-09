53131d30914f0cc04d6af56095db9c72805cdca1
bolt9ra1234 <bolt9ra1234@mail.ru>
2025-10-09 18:34:04 +0300

    Updated CartButton.tsx

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -9,11 +9,11 @@ const CartButton: React.FC = () => {
   const { data: cartData, isLoading, error } = useGetCartQuery();
 
   // API возвращает массив корзин, берем первую (активную)
-  const cart = cartData && cartData.length > 0 ? cartData[0] : null;
+  const cart = cartData && cartData.?items.length > 0 ? cartData.?items[0] : null;
   const totalItems = cart?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
 
   // Добавим логирование для отладки
-  console.log('CartButton - cartData:', cartData);
+  console.log('cart:', cart);
   console.log('CartButton - cart:', cart);
   console.log('CartButton - totalItems:', totalItems);
 
