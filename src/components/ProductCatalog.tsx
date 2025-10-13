9e404619f2c45c0aee6e7e50ccfeaf799d738ad0
bolt9ra1234 <bolt9ra1234@mail.ru>
2025-10-13 15:41:01 +0300

    Updated ProductCatalog.tsx

--- src/components/ProductCatalog.tsx
+++ src/components/ProductCatalog.tsx
@@ -33,7 +33,7 @@ const ProductCatalog: React.FC<ProductCatalogProps> = ({ selectedCategory }) => 
         Каталог
       </Typography>
       
- <Grid container spacing={2}>
+<Grid container spacing={2}>
   {products?.map((product) => (
     <Grid item xs={6} sm={6} md={4} lg={3} key={product.id}>
       <ProductCard product={product} />
